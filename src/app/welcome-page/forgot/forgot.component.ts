43951fc26c7454535580172927d3df82033c1b46
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2025-01-05 00:11:22 +0100

    forgot finalize

--- src/app/welcome-page/forgot/forgot.component.ts
+++ src/app/welcome-page/forgot/forgot.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ApiConnectServiceService } from '../../api-connect-service.service';
+import { ForgotPasswordRequestModel } from '../../Models/ForgotPasswordRequestModel ';
 
 @Component({
   selector: 'app-forgot',
@@ -12,16 +14,28 @@ import { FormsModule } from '@angular/forms';
 export class ForgotComponent {
   email: string = '';
   errorMessage: string = '';
-  successMessage: string = ''; 
+  successMessage: string = '';
+
+  constructor(private apiService: ApiConnectServiceService) {}
 
   onSend() {
     if (!this.email) {
       this.errorMessage = 'Please provide an email.';
       this.successMessage = '';
-    } else {
-      this.errorMessage = '';
-      this.successMessage =
-        'If there was an account with this email, a reset link has been sent.';
+      return;
     }
+
+    const request: ForgotPasswordRequestModel = { email: this.email };
+
+    this.apiService.forgotPassword(request).subscribe({
+      next: (response) => {
+        this.successMessage = response.message || 'A reset link has been sent to your email.';
+        this.errorMessage = '';
+      },
+      error: (error) => {
+        this.errorMessage = error.error?.message || 'Failed to send reset link. Please try again.';
+        this.successMessage = '';
+      },
+    });
   }
 }
