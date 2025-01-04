9b5c722c995a82e13adc10e8f205e1d5506721d6
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2025-01-04 06:18:26 +0100

    login finalize

--- src/app/welcome-page/login/login.component.ts
+++ src/app/welcome-page/login/login.component.ts
@@ -1,26 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { ApiConnectServiceService } from '../../api-connect-service.service';
+import { LoginRequestModel } from '../../Models/LoginRequestModel ';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [FormsModule, CommonModule, RouterModule], 
+  imports: [FormsModule, CommonModule, RouterModule],
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = ''; 
+  loginData: LoginRequestModel = {
+    login: '',
+    password: '',
+  };
   errorMessage: string = '';
 
+  constructor(private apiService: ApiConnectServiceService, private router: Router) {}
+
   onLogin() {
-    if (!this.email || !this.password) {
+    if (!this.loginData.login || !this.loginData.password) {
       this.errorMessage = 'Please fill both fields';
-    } else {
-      this.errorMessage = '';
-      console.log('Logging in with', this.email, this.password);
+      return;
     }
+
+    this.apiService.login(this.loginData).subscribe({
+      next: () => {
+        this.errorMessage = '';
+        this.router.navigate(['/dashboard']); 
+      },
+      error: (error) => {
+        this.errorMessage = error.error?.message || 'Login failed. Please try again.';
+      },
+    });
   }
 }
