645dd469ad5cfeb9d8378f31d9aed2ee4850a25e
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2025-01-04 02:55:47 +0100

    register finalize

--- src/app/welcome-page/register/register.component.ts
+++ src/app/welcome-page/register/register.component.ts
@@ -2,31 +2,58 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { ApiConnectServiceService } from '../../api-connect-service.service'; 
+import { RegistrationRequestModel } from '../../Models/RegistrationRequestModel ';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, CommonModule, RouterModule],
+  imports: [FormsModule, CommonModule, RouterModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  email: string = '';
-  password: string = '';
+  registrationData: RegistrationRequestModel = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
   confirmPassword: string = '';
   errorMessage: string = '';
   successMessage: string = '';
 
+  constructor(private apiService: ApiConnectServiceService, private router: Router) {}
+
   onRegister() {
-    if (!this.email || !this.password || !this.confirmPassword) {
+
+    if (!this.registrationData.username || !this.registrationData.email || !this.registrationData.password || !this.confirmPassword) {
       this.errorMessage = 'All fields are required';
       this.successMessage = '';
-    } else if (this.password !== this.confirmPassword) {
+      return;
+    }
+
+    if (this.registrationData.password !== this.confirmPassword) {
       this.errorMessage = 'Passwords do not match';
       this.successMessage = '';
-    } else {
-      this.errorMessage = '';
-      this.successMessage = 'Confirmation link has been sent!';
+      return;
     }
+
+    this.apiService.register(this.registrationData).subscribe({
+      next: this.handleSuccess.bind(this),
+      error: this.handleError.bind(this)
+    });
+  }
+
+  private handleSuccess() {
+    this.successMessage = 'Registration successful! Confirm your email.';
+    this.errorMessage = '';
+    setTimeout(() => this.router.navigate(['../login']), 2000);
+  }
+
+  private handleError(error: any) {
+    this.errorMessage = error.error?.message || 'Registration failed. Please try again.';
+    this.successMessage = '';
   }
 }
