656df9a074283d9595dcc0433f28f0bb531a21fa
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2025-01-07 03:48:57 +0100

    get Tokens, get liquidity Pools, baseUrl fix, account methods url fix

--- src/app/api-connect-service.service.ts
+++ src/app/api-connect-service.service.ts
@@ -5,29 +5,40 @@ import { LoginRequestModel } from './Models/LoginRequestModel ';
 import { ForgotPasswordRequestModel } from './Models/ForgotPasswordRequestModel ';
 import { RegistrationRequestModel } from './Models/RegistrationRequestModel ';
 import { ResetPasswordRequestModel } from './Models/ResetPasswordRequestModel';
+import { TokenCacheDto } from './Models/TokenCacheDto';
+import { LiquidityPool } from './Models/LiquidityPool';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiConnectServiceService {
-  private apiBaseUrl = 'https://localhost:7098/api/account';
+  private apiBaseUrl = 'https://localhost:7098/api';
 
   constructor(private http: HttpClient) {}
 
   login(loginRequest: LoginRequestModel): Observable<any> {
-    return this.http.post(`${this.apiBaseUrl}/Login`, loginRequest);
+    return this.http.post(`${this.apiBaseUrl}/account/Login`, loginRequest);
   }
 
   register(registrationRequest: RegistrationRequestModel): Observable<any> {
-    return this.http.post(`${this.apiBaseUrl}/Register`, registrationRequest);
+    return this.http.post(`${this.apiBaseUrl}/account/Register`, registrationRequest);
   }
 
   forgotPassword(forgotPasswordRequest: ForgotPasswordRequestModel): Observable<any> {
-    return this.http.post(`${this.apiBaseUrl}/ForgotPassword`, forgotPasswordRequest);
+    return this.http.post(`${this.apiBaseUrl}/account/ForgotPassword`, forgotPasswordRequest);
   }
 
   resetPassword(email: string, token: string, resetPasswordRequest: ResetPasswordRequestModel): Observable<any> {
-    const url = `${this.apiBaseUrl}/ResetPassword?email=${encodeURIComponent(email)}&token=${encodeURIComponent(token)}`;
+    const url = `${this.apiBaseUrl}/account/ResetPassword?email=${encodeURIComponent(email)}&token=${encodeURIComponent(token)}`;
     return this.http.post(url, resetPasswordRequest);
+  }
+
+  getTokens(): Observable<TokenCacheDto[]> {
+    return this.http.get<TokenCacheDto[]>(`${this.apiBaseUrl}/cache/Tokens`);
+  }
+
+  getLiquidityPools(): Observable<LiquidityPool[]> {
+    return this.http.get<LiquidityPool[]>(`${this.apiBaseUrl}/cache/LiquidityPools`);
   }
 }
