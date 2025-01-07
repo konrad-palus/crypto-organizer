51a1fee3b04f3c7dbecaf27e914f974639c0cf4c
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2025-01-07 03:46:14 +0100

    dashboard routeroutlet, css fix , router links

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
