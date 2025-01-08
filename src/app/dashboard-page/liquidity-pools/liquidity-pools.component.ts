4e0501ee277500f40f6546003ec3572c23517584
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2025-01-08 07:02:11 +0100

    LP pools component finalized

--- src/app/dashboard-page/liquidity-pools/liquidity-pools.component.ts
+++ src/app/dashboard-page/liquidity-pools/liquidity-pools.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiConnectServiceService } from '../../api-connect-service.service';
+import { LiquidityPool } from '../../Models/LiquidityPool';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-liquidity-pools',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './liquidity-pools.component.html',
-  styleUrl: './liquidity-pools.component.scss'
+  styleUrls: ['./liquidity-pools.component.scss'],
 })
-export class LiquidityPoolsComponent {
+export class LiquidityPoolsComponent implements OnInit {
+  liquidityPools: LiquidityPool[] = [];
+  errorMessage: string = '';
 
+  constructor(private apiService: ApiConnectServiceService) {}
+
+  ngOnInit(): void {
+    this.loadLiquidityPools();
+  }
+
+  loadLiquidityPools(): void {
+    this.apiService.getLiquidityPools().subscribe({
+      next: (data) => {
+        const sortedPools = data.sort((a, b) => a.buyPrice - b.buyPrice);
+        const latestTimestamp = Math.max(...sortedPools.map((pool) => new Date(pool.lastUpdated).getTime()));
+
+        const freshPools = sortedPools.filter((pool) => latestTimestamp - new Date(pool.lastUpdated).getTime() <= 60000);
+        const olderPools = sortedPools.filter((pool) =>latestTimestamp - new Date(pool.lastUpdated).getTime() > 60000);
+
+        this.liquidityPools = [...freshPools, ...olderPools];
+        this.errorMessage = '';
+      },
+      error: (error) => {
+        this.errorMessage =
+          error.error?.message || 'Failed to load liquidity pools.';
+      },
+    });
+  }
 }
